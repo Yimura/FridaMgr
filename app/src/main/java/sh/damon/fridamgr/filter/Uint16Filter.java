723abd168195818d558b4492eff633d22f408d70
Yimura <24669514+Yimura@users.noreply.github.com>
2024-09-13 15:10:27 +0200

    fix(Uint16Filter): app crash when removing chars

--- app/src/main/java/sh/damon/fridamgr/filter/Uint16Filter.java
+++ app/src/main/java/sh/damon/fridamgr/filter/Uint16Filter.java
@@ -6,7 +6,12 @@ import android.text.Spanned;
 public class Uint16Filter implements InputFilter {
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-        int num = Integer.parseInt(source.toString());
+        String input = source.toString();
+        if (input.isEmpty()) {
+            return null;
+        }
+
+        int num = Integer.parseInt(input);
         if (num < 0 || num > 65_535) {
             return "";
         }
