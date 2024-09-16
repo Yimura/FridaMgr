c038f43da43c387bd173e3c63d662ebb5734f9a8
Yimura <24669514+Yimura@users.noreply.github.com>
2024-09-16 15:06:32 +0200

    fix(BootReceiver): load UserPreferences

--- app/src/main/java/sh/damon/fridamgr/receivers/BootReceiver.java
+++ app/src/main/java/sh/damon/fridamgr/receivers/BootReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.io.File;
+
 import sh.damon.fridamgr.FridaServer;
 import sh.damon.fridamgr.preferences.Preferences;
 import sh.damon.fridamgr.preferences.UserPreferences;
@@ -12,6 +14,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            UserPreferences.load(new File(context.getFilesDir(), "fridaMgr.json"));
             final FridaServer server = FridaServer.init(context.getFilesDir());
 
             if (UserPreferences.get(Preferences.START_ON_BOOT, false)) {
