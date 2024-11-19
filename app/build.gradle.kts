7c11af5398fc078ac261ae0e770670efcce391ad
Yimura <24669514+Yimura@users.noreply.github.com>
2024-11-19 14:01:45 +0100

    fix(Build): wrong version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 5
-        versionName = "0.1.5"
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
