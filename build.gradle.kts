cf156134011e58d239aeb3cfcb30b009e82f9f29
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2025-03-25 16:29:26 +0100

    feat(Gradle): setup sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "Yimura_FridaMgr_8180167f-e574-4541-bc70-1863a696da99")
+    property("sonar.projectName", "FridaMgr")
+  }
 }
